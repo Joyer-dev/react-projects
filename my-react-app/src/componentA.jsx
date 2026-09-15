8b8110de5a9e79e540c0084b81e313fa840cb67e
Joyer dev <joycewildad2@gmail.com>
2026-09-15 12:45:06 +0300

    applying createContext from react

--- my-react-app/src/componentA.jsx
+++ my-react-app/src/componentA.jsx
@@ -1,17 +1,16 @@
 import React, {useState,createContext} from 'react';
 import ComponentB from "./componentB.jsx";
+export const UserContext = createContext();
 
-export const UserContext =createContext();
-function ComponentA(props){
+function ComponentA(){
 
     const [user, setUser]=useState("Joleen")
     return(<div className="box">
         <h1>Component A</h1>
-        <h2>{`Hello ${user}`}</h2>
-        
+        <h2>{`Hellow ${user}`}</h2>
         <UserContext.Provider value={user}>
-        <ComponentB user={user}/>
+            <ComponentB user={user}/>
         </UserContext.Provider>
-    </div>)
+    </div>);
 }
 export default ComponentA
